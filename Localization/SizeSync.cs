9f7e8a24b326e80a746d35adc5bb34a73f8a6afd
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2011-11-25 12:51:04 -0600

    Added SizeSync class to bind autosized control sizes to one another

---
+++ Localization/SizeSync.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace NeoSmart.Localization
+{
+    public static class SizeSync
+    {
+        public static void Sync(IEnumerable<Control> controls, bool syncWidth, bool syncHeight)
+        {
+            foreach (var i in controls)
+            {
+                var i1 = i;
+                foreach (var j in controls)
+                {
+                    var j1 = j;
+                    if (i1 == j1)
+                        continue;
+
+                    var handlerDisabled = false;
+
+                    EventHandler handler = (sender, e) =>
+                    {
+                        if (syncWidth)
+                        {
+                            if (!handlerDisabled)
+                            {
+                                handlerDisabled = true;
+                                i1.Width = j1.Width;
+                                handlerDisabled = false;
+                            }
+                        }
+                        else if (syncHeight)
+                        {
+                            if (!handlerDisabled)
+                            {
+                                handlerDisabled = true;
+                                i1.Height = j1.Height;
+                                handlerDisabled = false;
+                            }
+                        }
+                    };
+
+                    j.Resize += handler;
+                }
+            }
+        }
+    }
+}
