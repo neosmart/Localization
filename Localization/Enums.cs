869323f646c790e3084964785e645b1e5d85fd7a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2011-11-26 01:45:36 -0600

    Added classes missing from last commit

---
+++ Localization/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoSmart.Localization
+{
+	public enum StringStatus
+	{
+		Synced,
+		Outdated,
+		Missing
+	}
+}
---
+++ Localization/StringTranslation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoSmart.Localization
+{
+	public class StringTranslation
+	{
+		public string Key { get; set; }
+
+		public string Value { get; set; }
+
+		public bool DeriveFromParent { get; set; }
+
+		public int Version { get; set; }
+
+		public StringTranslation(string key, string value)
+		{
+			Key = key;
+			Value = value;
+		}
+
+		public int BumpVersion()
+		{
+			return ++Version;
+		}
+	}
+}
